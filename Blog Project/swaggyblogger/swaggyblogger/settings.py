b9a681c676baff6b702d30f4decedf0295775839
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-23 01:43:21 -0700

    Update settings.py

--- Blog Project/swaggyblogger/swaggyblogger/settings.py
+++ Blog Project/swaggyblogger/swaggyblogger/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'blog.CustomUser'
